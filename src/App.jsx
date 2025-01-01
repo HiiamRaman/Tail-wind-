8e12dfb6daf64a20c0bd8e71794cfe26432be8d5
Raman Singh Chauhan <ramansinghchauhan787@gmail.com>
2025-01-01 15:09:45 +0545

    intial

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,43 @@
-import { useState } from "react";
 
-import "./App.css";
 
-function App() {
+
+
+export default function App() {
   return (
-    <div className="border-8 border-yellow-700">
-      <h1>Raman SIngh</h1>
-      <div className="bg-green-400 text-gray-950">
-        <p className="text-xs">Raman</p>
-        <p className="truncate ...">
+    // <div className="border-8 border-yellow-700">
+    //   <h1>Raman SIngh</h1>
+    //   <div className="bg-green-400 text-gray-950">
+    //     <p className="text-xs">Raman</p>
+    //     <p className="truncate ...">
          
-          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Iste
-          obcaecati itaque soluta asperiores tempore. Dolores nesciunt,
-          perspiciatis iusto magnam omnis obcaecati tempore exercitationem totam
-          voluptate ullam alias! Aliquid, soluta eaque.
-        </p>
-        <p className="text-clip overflow-hidden ...">
+    //       Lorem ipsum dolor sit amet consectetur, adipisicing elit. Iste
+    //       obcaecati itaque soluta asperiores tempore. Dolores nesciunt,
+    //       perspiciatis iusto magnam omnis obcaecati tempore exercitationem totam
+    //       voluptate ullam alias! Aliquid, soluta eaque.
+    //     </p>
+    //     <p className="text-clip overflow-hidden ...">
          
-          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Iste
-          obcaecati itaque soluta asperiores tempore. Dolores nesciunt,
-          perspiciatis iusto magnam omnis obcaecati tempore exercitationem totam
-          voluptate ullam alias! Aliquid, soluta eaque.
-        </p>
-      </div>
+    //       Lorem ipsum dolor sit amet consectetur, adipisicing elit. Iste
+    //       obcaecati itaque soluta asperiores tempore. Dolores nesciunt,
+    //       perspiciatis iusto magnam omnis obcaecati tempore exercitationem totam
+    //       voluptate ullam alias! Aliquid, soluta eaque.
+    //     </p>
+    //   </div>
+
+    //   <button className=" pt-10 Raman "> Billa </button>
+
+
+    // </div>
+    <div className=" flex w-full ">
+      <div className=" p-16  w-1/2 bg-red-200 text-green-950 ra "> go </div>   
+      <div className=" p-16  w-1/2 bg-blue-200 text-green-950  ba"> sds</div>
+      
     </div>
+
+
+
+      
+    
   );
 }
 
-export default App;
